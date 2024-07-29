fc2c014fe1ded405e7f907867f8f9bbff5d517db
Håkon Line <hakon.line@sporveien.com>
2024-07-29 15:48:19 +0200

    add token authentication to Databricks API call

--- bricker/__init__.py
+++ bricker/__init__.py
@@ -31,11 +31,18 @@ def path_from_local(path): return path.replace("\\","/").replace("./","")
 def path_from_dbc(path):   return path.replace(dbc_base(),"")
 
 def dbc(endpoint, json, ignored_errors=[]):
-    api_method = requests.get if endpoint in ['workspace/list','workspace/export','clusters/list-zones','clusters/spark-versions'] else requests.post
-    if os.environ.get('DBC_USER'):
+    if endpoint in ['workspace/list','workspace/export','clusters/list-zones','clusters/spark-versions']:
+        api_method = requests.get
+    else:
+        api_method = requests.post
+
+    if os.environ.get('DBC_TOKEN'):
+        res = api_method(settings()['api_url'] + endpoint, json=json, headers={'Authorization': f'Bearer {os.environ["DBC_TOKEN"]}'})
+    elif os.environ.get('DBC_USER'):
         res = api_method(settings()['api_url'] + endpoint, json=json, auth=(os.environ['DBC_USER'],os.environ['DBC_PASS']))
     else:
         res = api_method(settings()['api_url'] + endpoint, json=json)
+
     if res.status_code == 401:
         raise click.ClickException("Unauthorized call to Databricks (remember to create a netrc file to authenticate with DBC - and check if your user/pass is the one used for DBC)")
     if res.status_code == requests.codes.ok or res.json()["error_code"] in ignored_errors:
